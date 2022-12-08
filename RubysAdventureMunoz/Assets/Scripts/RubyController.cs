1beac03a1819dff46866328f4ab5101cfa087668
Sarah Munoz <smbabbs06@gmail.com>
2022-12-08 09:58:45 -0800

    starting animations
    added robot qwerty.

--- RubysAdventureMunoz/Assets/Scripts/RubyController.cs
+++ RubysAdventureMunoz/Assets/Scripts/RubyController.cs
@@ -7,6 +7,7 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
 
     public int health { get { return currentHealth;  } }
     int currentHealth;
@@ -42,15 +43,15 @@ public class RubyController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
 
     public void ChangeHealth(int amount)
     {
-        if(amount < 0)
+        if (amount < 0)
         {
             if (isInvincible)
                 return;
