4b193c8c36cceed23f69651e74a8ec85c9963cda
Sarah Munoz <smbabbs06@gmail.com>
2022-12-12 09:59:57 -0800

    Starting animations
    Added animation to qwerty and ruby.

--- RubysAdventureMunoz/Assets/Scripts/EnemyController.cs
+++ RubysAdventureMunoz/Assets/Scripts/EnemyController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed = 3.0f;
+    public float speed;
     public bool vertical;
     public float changeTime = 3.0f;
 
@@ -12,11 +12,13 @@ public class EnemyController : MonoBehaviour
     float timer;
     int direction = 1;
 
-    // Start is called before the first frame update
+    Animator animator;
+
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -36,10 +38,14 @@ public class EnemyController : MonoBehaviour
 
         if (vertical)
         {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
             position.y = position.y + Time.deltaTime * speed * direction; ;
         }
         else
         {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
             position.x = position.x + Time.deltaTime * speed * direction; ;
         }
 
--- RubysAdventureMunoz/Assets/Scripts/RubyController.cs
+++ RubysAdventureMunoz/Assets/Scripts/RubyController.cs
@@ -19,11 +19,16 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
-    // Start is called before the first frame update
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
         currentHealth = maxHealth;
+
     }
 
     // Update is called once per frame
@@ -31,6 +36,18 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -53,6 +70,7 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
 
